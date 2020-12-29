2eed4d4cd0fd9b3a483ca1244531ad621c460a54
lichao <979071224@qq.com>
2020-12-29 11:36:14 +0800

    update

--- buy.go
+++ buy.go
@@ -1205,6 +1205,7 @@ func dogbaby_list(configuration st.Configuration) string {
 		if (res != "") {
 			fmt.Print("\n")
 			fmt.Print("繁殖进行中...", time.Now())
+			fmt.Print("\n")
 		}
 		return res
 	case <-time.After(dog_list_timeout * time.Second):
